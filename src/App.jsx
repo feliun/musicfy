64aa5a69885034e228cc88a2e73309d579ed2e35
feliun <felipe.polo@gmail.com>
2018-05-29 19:29:25 +0200

    Some more styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 class App extends Component {
 	render() {
 		return (
-			<div>
+			<div id="app">
 				<header id="app-title">Musicfy</header>
-				<section>
-					<input placeholder="Search for an artist..." />
-					<button>Search</button>
-				</section>
+				<FormGroup>
+					<InputGroup className="search-element">
+						<FormControl id="search-bar" type="text" placeholder="Search for an artist..." />
+					</InputGroup>
+					<InputGroup.Addon>
+						<Glyphicon id="search-button" glyph="search" />
+					</InputGroup.Addon>
+				</FormGroup>
 				<section id="profile">
 					<div>Artist picture</div>
 					<div>Artist name</div>
