e4f2908b7d15f8f371c21d0758288db4f983d427
feliun <felipe.polo@gmail.com>
2018-05-30 18:04:18 +0200

    Doing singer search

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,49 @@
-import React, { Component } from 'react';
-import './App.css';
-import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
+import React, { Component } from "react";
+import "./App.css";
+import { FormGroup, FormControl, InputGroup, Glyphicon } from "react-bootstrap";
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			query: "",
+		};
+	}
+
+	search() {
+		const BASE_URL = "https://api.spotify.com/v1/search";
+		const FETCH_URL = `${BASE_URL}?q=${this.state.query}&type=artist&limit=1`;
+		const token = "";
+		fetch(FETCH_URL, {
+			method: "GET",
+			headers: new Headers({
+				Authorization: `Bearer ${token}`,
+				"Content-Type": "application/x-www-form-urlencoded",
+			}),
+		})
+			.then(response => response.json())
+			.then(json => console.log(json))
+			.catch(error => console.log(error));
+	}
+
 	render() {
 		return (
 			<div id="app">
 				<header id="app-title">Musicfy</header>
 				<FormGroup>
 					<InputGroup className="search-element">
-						<FormControl id="search-bar" type="text" placeholder="Search for an artist..." />
+						<FormControl
+							id="search-bar"
+							type="text"
+							query={this.state.query}
+							placeholder="Search for an artist..."
+							onChange={event => this.setState({ query: event.target.value })}
+							onKeyPress={event => {
+								if (event.key === "Enter") this.search();
+							}}
+						/>
 					</InputGroup>
-					<InputGroup.Addon>
+					<InputGroup.Addon onClick={() => this.search()}>
 						<Glyphicon id="search-button" glyph="search" />
 					</InputGroup.Addon>
 				</FormGroup>
