7d89d46dcb2d87b5d4d7442cb247d3a65655e876
feliun <felipe.polo@gmail.com>
2018-05-31 15:21:04 +0200

    Playing/pausing songs

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -2,6 +2,38 @@ import React, { Component } from "react";
 import "./App.css";
 
 class Gallery extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			playingUrl: "",
+			audio: null,
+			playing: false,
+		};
+	}
+
+	playAudio(url) {
+		const audio = new Audio(url);
+		if (!this.state.playing) {
+			audio.play();
+			return this.setState({
+				playing: true,
+				playingUrl: url,
+				audio,
+			});
+		}
+		if (url === this.state.playingUrl) {
+			this.state.audio.pause();
+			return this.setState({ playing: false });
+		}
+		this.state.audio.pause();
+		audio.play();
+		this.setState({
+			playing: true,
+			audio,
+			playingUrl: url,
+		});
+	}
+
 	render() {
 		const { tracks } = this.props;
 		return (
@@ -10,7 +42,7 @@ class Gallery extends Component {
 					const trackImg = track.album.images[0].url;
 					return (
 						<div key={i} className="track">
-							<img src={trackImg} alt="track" className="track-img" />
+							<img src={trackImg} alt="track" className="track-img" onClick={() => this.playAudio(track.preview_url)} />
 							<p className="track-text">{track.name}</p>
 						</div>
 					);
