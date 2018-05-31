285c9812fded7dfc402fa5b0eb3264df5d4d5446
feliun <felipe.polo@gmail.com>
2018-05-31 09:41:48 +0200

    Loading cached data to speed up development

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import { FormGroup, FormControl, InputGroup, Glyphicon } from "react-bootstrap";
 import Profile from "./Profile";
+import BeatlesData from "./fixtures/beatles.json";
 
 class App extends Component {
 	constructor(props) {
@@ -15,21 +16,22 @@ class App extends Component {
 	search() {
 		const BASE_URL = "https://api.spotify.com/v1/search";
 		const FETCH_URL = `${BASE_URL}?q=${this.state.query}&type=artist&limit=1`;
-		const token =
-			"BQD0QujaOPoNfMMTaTE2jay3f9QHA_gSHuvBedk2Cxacd9B3Xt1tEkqZWeaaBZM6A7-BzhwlskB4TlyQ8jL3Fedyy9RN6py4LoZ-mo1r8ia2iCoeWkW5oNFQaYjjFOThXnK65uzDYIsVtYzEKSpH3BZSK3qUL2b4";
-		fetch(FETCH_URL, {
-			method: "GET",
-			headers: new Headers({
-				Authorization: `Bearer ${token}`,
-				"Content-Type": "application/x-www-form-urlencoded",
-			}),
-		})
-			.then(response => response.json())
-			.then(({ artists: { items } }) => {
-				const artist = items[0];
-				this.setState({ artist });
-			})
-			.catch(error => console.log(error));
+		const token = "";
+		const artist = BeatlesData;
+		this.setState({ artist });
+		// fetch(FETCH_URL, {
+		// 	method: "GET",
+		// 	headers: new Headers({
+		// 		Authorization: `Bearer ${token}`,
+		// 		"Content-Type": "application/x-www-form-urlencoded",
+		// 	}),
+		// })
+		// 	.then(response => response.json())
+		// 	.then(({ artists: { items } }) => {
+		// 		const artist = items[0];
+		// 		this.setState({ artist });
+		// 	})
+		// 	.catch(error => console.log(error));
 	}
 
 	render() {
